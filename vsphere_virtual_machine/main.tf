fa37dcb19bf0fcf347fb4e95313e88ed0c6fc45a
Quintella Rosa <132625335+fabioaurelioqrosa@users.noreply.github.com>
2023-09-07 19:57:04 +0000

    .

---
+++ vsphere_virtual_machine/main.tf
@@ -0,0 +1,76 @@
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_host" "host" {
+ // name          = var.vsphere_host
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_resource_pool" "main" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+
+  name             = var.name
+
+  resource_pool_id = data.vsphere_resource_pool.main.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  
+  firmware         = var.firmware
+  num_cpus         = var.num_cpus
+  memory           = var.memory
+  guest_id         = var.guest_id
+
+  //run_once_command_list = var.run_once_command_list
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  #Storage
+  disk {
+    label            = var.disk_label 
+    size             = var.disk_size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+
+      linux_options {
+        host_name = var.host_name
+        domain    = var.domain
+      }
+
+      network_interface {
+        ipv4_address = var.ipv4_address
+        ipv4_netmask = var.ipv4_netmask
+      }
+      ipv4_gateway    = var.ipv4_gateway
+      dns_server_list = var.dns_server_list
+    }
+  }
+}/**/
---
+++ vsphere_virtual_machine/variables.tf
@@ -0,0 +1,108 @@
+variable "vsphere_datacenter" {
+  type    = string
+  default = "IT Labs"
+}
+
+variable "vsphere_resource_pool" {
+  type    = string
+}
+
+variable "vsphere_host" {
+  type    = string
+  default = "esxi-2696v4.itlabs.local"
+}
+
+variable "vsphere_datastore" {
+  type    = string
+  default = "Samsung SSD 850 EVO 500 GB"
+}
+
+variable "vsphere_network" {
+  type    = string
+  default = "VM Network"
+}
+
+variable "template" {
+  type    = string
+  default = "Oracle Linux 8.7"
+}
+
+variable "name" {
+  type    = string
+}
+
+variable "firmware" {
+  type    = string
+  default = "efi"
+}
+
+variable "num_cpus" {
+  type    = number
+  default = 2
+}
+
+variable "memory" {
+  type    = number
+  default = 2048
+}
+
+variable "guest_id" {
+  type    = string
+  default = "oracleLinux8_64Guest"
+}
+
+variable "use_static_mac" {
+  type    = bool
+  default = false
+}
+
+variable "mac_address" {
+  type    = string
+  default = null
+}
+
+variable "disk_label" {
+  type    = string
+}
+
+variable "disk_size" {
+  type    = number
+  default = 20
+}
+
+variable "host_name" {
+  type    = string
+}
+
+variable "domain" {
+  type    = string
+  default = "itlabs.local"
+}
+
+variable "ipv4_address" {
+  type    = string
+  default = null
+}
+
+
+variable "ipv4_netmask" {
+  type    = number
+  default = 24
+}
+
+
+variable "ipv4_gateway" {
+  type    = string
+  default = "192.168.0.1"
+}
+
+
+variable "dns_server_list" {
+  type    = list(string)
+  default = ["192.168.0.2"]
+}
+
+variable "run_once_command_list" {
+  type    = list(string)
+  default = null
+}
